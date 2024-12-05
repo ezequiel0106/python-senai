056a510f06e3d5a8d600cc6cae16f9d1425f8b33
ezequiel0106 <ezequiel.feliciano2020@gmail.com>
2024-12-05 09:55:50 -0300

    Add files via upload

---
+++ Aula2/Atividade2.py
@@ -0,0 +1,8 @@
+print('Bom dia a todos!')
+
+nome_usuario = 'Ezequiel' # Tipo -> str == string
+idade_usuario = 18 # Tipo  -> int == integer
+altura_usuario = 1.72 # Tipo -> float == ponto flutuante
+maior_idade = True # Tipo -> bool == boolean
+
+print('Seja bem-vindo', nome_usuario,'sua idade é', idade_usuario, 'anos. Você tem', altura_usuario, 'metros. Você é maior de idade?', maior_idade)
